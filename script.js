004ed03de41602b678644472f02fbe062c8fa0e5
borakcstborak <mdislamshakib218@gmail.com>
2024-09-17 14:59:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// const p = document.getElementsByTagName('p');
+// for(const body of p){
+//     body.style.border = '2px solid black';
+//     body.style.textAlign = 'center';
+//     body.style.fontSize = '100px';
+//     body.style.backgroundColor = 'crimson';
+// }
+
+
+
+// const bbb = document.getElementsByClassName('bbb');
+// for(const body of bbb){
+//     body.style.border = '2px solid black';
+//     body.style.textAlign = 'center';
+//     body.style.fontSize = '50px';
+//     body.style.backgroundColor = 'gray';
+//     body.style.color = 'red';
+// }
+
+
+
+
+// const ul = document.getElementsByTagName('ul');
+// for(const body of ul){
+//     body.style.fontSize = '50px';
+//     body.style.color = 'red';
+// }
+
+
+// const abc = document.getElementsByClassName('abc');
+// for(const body of abc){
+//     body.style.fontSize = '150px';
+//     body.style.color = 'blue';
+// }
+
+
+// const xyz = document.getElementById('xyz');
+//     for (const body of xyz){
+//         body.style.fontSize ='48px';
+//     }
+
+
+// const button =document.getElementById('switch');
+//  button .onclick = changeBackground;
+//  function  changeBackground (){
+//     document.body.style.backgroundColor = 'orange';
+//  };
+   
+
+{/* <button id="mynumber">my phone number showing clickthis button </button> */}
